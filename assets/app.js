8beb5de3a3ad047217d34c2a487ebb1e82c48d83
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2025-09-17 16:11:27 +0200

    css refactoring

--- assets/app.js
+++ assets/app.js
@@ -1,5 +1,6 @@
 import './bootstrap.js';
 import './styles/global.css';
 import './styles/nav.css';
-import './styles/dashboard.css'
-import './styles/courses.css'
+import './styles/dashboard.css';
+import './styles/courses.css';
+import './styles/progressTracking.css';
