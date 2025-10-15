9d702f0d96f8dc0df83f49455567632ba90a4594
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2025-10-15 11:13:19 +0200

    subscription price prod

--- src/Controller/SubscriptionController.php
+++ src/Controller/SubscriptionController.php
@@ -39,8 +39,8 @@ final class SubscriptionController extends AbstractController
         \Stripe\Stripe::setApiKey($_SERVER['STRIPE_SECRET_KEY'] ?? $_ENV['STRIPE_SECRET_KEY']);
 
         // ⚙️ ID du plan Stripe (récurrent)
-        # $priceId = 'price_1SFEVoGYlvTXCIGyR8dbgFLA'; // prod price
-        $priceId = 'price_1SFsWLGYlvTXCIGyJNib1Bi6'; // test price
+        $priceId = 'price_1SFEVoGYlvTXCIGyR8dbgFLA'; // prod price
+        #$priceId = 'price_1SFsWLGYlvTXCIGyJNib1Bi6'; // test price
         
 
         $session = \Stripe\Checkout\Session::create([
