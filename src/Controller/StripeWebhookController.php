e73870a2822abb49d2ea058c34242fee9f13a36f
Nikoloz <nikoloz.mamyan@lnstrade.fr>
2025-10-15 11:11:07 +0200

    email testing6

--- src/Controller/StripeWebhookController.php
+++ src/Controller/StripeWebhookController.php
@@ -16,6 +16,7 @@ use App\Repository\SubscriptionRepository;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\Routing\Generator\UrlGeneratorInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 
 class StripeWebhookController extends AbstractController
